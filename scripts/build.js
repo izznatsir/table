e32e2d3a8f27eabbfc9305a0454b17e133e23aa3
Izzuddin Natsir <izznatsir@gmail.com>
2022-04-10 11:01:46 +0700

    build both cjs and mjs, and change package type to commonjs

--- scripts/build.js
+++ scripts/build.js
@@ -1,29 +1,38 @@
 import { build } from 'esbuild'
 import babel from '@chialab/esbuild-plugin-babel'
 
-async function main() {
-    await build({
-        bundle: true,
-        entryPoints: ['packages/table-core/src/index.ts'],
-        format: 'esm',
-        outdir: 'packages/table-core/dist',
-        sourcemap: true,
-        write: true,
-    })
+const formats = [
+    ['cjs', '.cjs'],
+    ['esm', '.mjs'],
+]
 
-    await build({
-        bundle: true,
-        entryPoints: ['packages/table-solid/src/index.tsx'],
-        external: ['solid-js'],
-        format: 'esm',
-        outdir: 'packages/table-solid/dist',
-        plugins: [
-            babel({
-                presets: ['@babel/preset-typescript', 'babel-preset-solid'],
-            }),
-        ],
-        sourcemap: true,
-        write: true,
+function main() {
+    formats.forEach(async (format) => {
+        await build({
+            bundle: true,
+            entryPoints: ['packages/table-core/src/index.ts'],
+            format: format[0],
+            outdir: 'packages/table-core/dist',
+            outExtension: { '.js': format[1] },
+            sourcemap: true,
+            write: true,
+        })
+
+        await build({
+            bundle: true,
+            entryPoints: ['packages/table-solid/src/index.tsx'],
+            external: ['solid-js'],
+            format: format[0],
+            outdir: 'packages/table-solid/dist',
+            outExtension: { '.js': format[1] },
+            plugins: [
+                babel({
+                    presets: ['@babel/preset-typescript', 'babel-preset-solid'],
+                }),
+            ],
+            sourcemap: true,
+            write: true,
+        })
     })
 }
 
