fda473b41f8ed664c3a6dca970fd076b4fefc244
Izzuddin Natsir <izznatsir@gmail.com>
2022-04-10 08:59:49 +0700

    example: add solid basic example

---
+++ examples/solid/basic/src/app.tsx
@@ -0,0 +1,146 @@
+import { createSignal, For } from 'solid-js'
+
+import { createTable, useTable } from '@izznatsir/table-solid'
+
+type Person = {
+    firstName: string
+    lastName: string
+    age: number
+    visits: number
+    status: string
+    progress: number
+}
+
+const defaultData: Person[] = [
+    {
+        firstName: 'tanner',
+        lastName: 'linsley',
+        age: 24,
+        visits: 100,
+        status: 'In Relationship',
+        progress: 50,
+    },
+    {
+        firstName: 'tandy',
+        lastName: 'miller',
+        age: 40,
+        visits: 40,
+        status: 'Single',
+        progress: 80,
+    },
+    {
+        firstName: 'joe',
+        lastName: 'dirte',
+        age: 45,
+        visits: 20,
+        status: 'Complicated',
+        progress: 10,
+    },
+]
+
+const table = createTable<{ Row: Person }>()
+
+const defaultColumns = table.createColumns([
+    table.createGroup({
+        header: 'Name',
+        footer: (props) => props.column.id,
+        columns: [
+            table.createDataColumn('firstName', {
+                cell: (info) => info.value,
+                footer: (props) => props.column.id,
+            }),
+            table.createDataColumn((row) => row.lastName, {
+                id: 'lastName',
+                cell: (info) => info.value,
+                header: () => <span>Last Name</span>,
+                footer: (props) => props.column.id,
+            }),
+        ],
+    }),
+    table.createGroup({
+        header: 'Info',
+        footer: (props) => props.column.id,
+        columns: [
+            table.createDataColumn('age', {
+                header: () => 'Age',
+                footer: (props) => props.column.id,
+            }),
+            table.createGroup({
+                header: 'More Info',
+                columns: [
+                    table.createDataColumn('visits', {
+                        header: () => <span>Visits</span>,
+                        footer: (props) => props.column.id,
+                    }),
+                    table.createDataColumn('status', {
+                        header: 'Status',
+                        footer: (props) => props.column.id,
+                    }),
+                    table.createDataColumn('progress', {
+                        header: 'Profile Progress',
+                        footer: (props) => props.column.id,
+                    }),
+                ],
+            }),
+        ],
+    }),
+])
+
+export default function App() {
+    const [data] = createSignal([...defaultData])
+    const [columns] = createSignal<typeof defaultColumns>([...defaultColumns])
+
+    const instance = useTable(table, {
+        data: data(),
+        columns: columns(),
+    })
+
+    return (
+        <div className="p-2">
+            <table {...instance.getTableProps()}>
+                <thead>
+                    <For each={instance.getHeaderGroups()}>
+                        {(headerGroup) => (
+                            <tr {...headerGroup.getHeaderGroupProps()}>
+                                <For each={headerGroup.headers}>
+                                    {(header) => (
+                                        <th {...header.getHeaderProps()}>
+                                            {header.isPlaceholder ? null : header.renderHeader()}
+                                        </th>
+                                    )}
+                                </For>
+                            </tr>
+                        )}
+                    </For>
+                </thead>
+                <tbody {...instance.getTableBodyProps()}>
+                    <For each={instance.getRowModel().rows}>
+                        {(row) => (
+                            <tr {...row.getRowProps()}>
+                                {row.getVisibleCells().map((cell) => (
+                                    <td {...cell.getCellProps()}>{cell.renderCell()}</td>
+                                ))}
+                            </tr>
+                        )}
+                    </For>
+                </tbody>
+                <tfoot>
+                    <For each={instance.getFooterGroups()}>
+                        {(footerGroup) => (
+                            <tr {...footerGroup.getFooterGroupProps()}>
+                                <For each={footerGroup.headers}>
+                                    {(header) => (
+                                        <th {...header.getFooterProps()}>
+                                            {header.isPlaceholder ? null : header.renderFooter()}
+                                        </th>
+                                    )}
+                                </For>
+                            </tr>
+                        )}
+                    </For>
+                </tfoot>
+            </table>
+            <div className="h-4" />
+        </div>
+    )
+}
---
+++ examples/solid/basic/src/main.tsx
@@ -0,0 +1,10 @@
+import './main.css'
+
+import { render } from 'solid-js/web'
+import App from './app'
+
+const root = document.getElementById('root')
+
+if (root) {
+  render(() => <App />, root)
+}
